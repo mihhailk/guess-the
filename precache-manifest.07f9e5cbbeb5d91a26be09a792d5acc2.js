7652676f04e33d4283b5405a2c250e4a8e2c40e9
Mihhail Kornõšev <mihhail@codeborne.com>
2020-01-16 22:37:52 +0200

    Updates

---
+++ precache-manifest.07f9e5cbbeb5d91a26be09a792d5acc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853a328ccd80c1471c8fd4773a3fdb55",
+    "url": "/guess-the/index.html"
+  },
+  {
+    "revision": "b6c7f1b0270ae46d5293",
+    "url": "/guess-the/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b6c7f1b0270ae46d5293",
+    "url": "/guess-the/static/js/2.9c03532d.chunk.js"
+  },
+  {
+    "revision": "e76f6fc05df4c6012490e86628397d0a",
+    "url": "/guess-the/static/js/2.9c03532d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd3f558f483c27d9452a",
+    "url": "/guess-the/static/js/main.d4f1f861.chunk.js"
+  },
+  {
+    "revision": "0125f2fc14905a63b459",
+    "url": "/guess-the/static/js/runtime-main.ad26ad52.js"
+  }
+]);
