c670181ac28295698c96fbc44a1e5c653b3663e7
Mihhail Kornõšev <mihhail@codeborne.com>
2020-01-18 09:42:46 +0200

    CRy9ehIC show page header with statistics even when answers are not given yet

--- src/App.test.js
+++ src/App.test.js
@@ -2,37 +2,7 @@ import React from 'react';
 import {mount, shallow} from 'enzyme';
 import mockData from './mock-si.json';
 import App from './App';
-import {PageHeader} from './components/PageHeader';
 import {Input, Label} from 'reactstrap';
-
-describe('headers', () => {
-  it('renders header on first page', () => {
-    const app = shallow(<App data={mockData}/>);
-
-    const pageHeader = app.find(PageHeader);
-    expect(pageHeader.props().totalNumberOfAnswers).toEqual(0);
-    expect(pageHeader.props().totalNumberOfCorrectAnswers).toEqual(0);
-    expect(pageHeader.props().numberOfAuthorsLeft).toEqual(mockData.length);
-  });
-
-  it('renders header on next page', () => {
-    const mockRandomQuestion = jest.fn(() => {
-      return mockData[0];
-    });
-    App.prototype.randomQuestion = mockRandomQuestion;
-
-    const app = mount(<App data={mockData}/>);
-
-    app.find('Button#next').simulate('click');
-    expect(mockRandomQuestion).toHaveBeenCalled();
-    expect(app.state().totalNumberOfCorrectAnswers).toEqual(0);
-    expect(app.state().totalNumberOfAnswers).toEqual(2);
-    const pageHeader = app.find(PageHeader);
-    expect(pageHeader.props().totalNumberOfAnswers).toEqual(2);
-    expect(pageHeader.props().totalNumberOfCorrectAnswers).toEqual(0);
-    expect(pageHeader.props().numberOfAuthorsLeft).toEqual(mockData.length - 1);
-  });
-});
 
 describe('author and creations', () => {
   it('renders author name correctly', () => {
---
+++ src/components/PageHeader.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import {PageHeader} from './PageHeader';
+import {Badge, NavbarBrand} from 'reactstrap';
+
+describe('PageHeader', () => {
+  it('renders game statistics', () => {
+    const app = mount(<PageHeader numberOfAuthorsLeft={9} totalNumberOfAnswers={8} totalNumberOfCorrectAnswers={7}/>);
+
+    const badges = app.find(Badge);
+    expect(badges.at(0).text()).toEqual("Всего: 8");
+    expect(badges.at(1).text()).toEqual("Верных: 7");
+    expect(badges.at(2).text()).toEqual("Осталось авторов: 9");
+  });
+
+  it('renders navigation bar brand', () => {
+    const app = mount(<PageHeader numberOfAuthorsLeft={6} totalNumberOfAnswers={5} totalNumberOfCorrectAnswers={4}/>);
+
+    expect(app.find(NavbarBrand).text()).toEqual("Это свояк");
+  });
+});
